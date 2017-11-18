0cf53dc5c8ae369f1eff9c2ed5312005b7ecbbfc
visidh <s8visidh@stud.uni-saarland.de>
2017-11-18 20:04:42 +0100

    Initial Commit

---
+++ Learning Cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Learning Cpp
+//
+//  Created by Vikramjit Sidhu on 18/11/17.
+//  Copyright © 2017 Vikramjit Sidhu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
